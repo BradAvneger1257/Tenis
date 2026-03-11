c1ed6f319fb025e78a6ef4747c4bb55a3b434937
JaviIG21 <adolfokataboom21@gmail.com>
2026-03-11 09:09:13 -0600

    Se agregó carrito de compras y botones agregar al carrito

--- bradley.js
+++ bradley.js
@@ -1,26 +1,58 @@
 const botones = document.querySelectorAll(".filtro-btn");
 const cards = document.querySelectorAll(".card");
 
+/* FILTRO DE MARCAS */
+
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
 
-        // Quitar active de todos
         botones.forEach(btn => btn.classList.remove("active"));
-
-        // Activar botón actual
         boton.classList.add("active");
 
         const marca = boton.getAttribute("data-marca");
 
         cards.forEach(card => {
+
             if (marca === "todos") {
                 card.style.display = "block";
-            } else {
+            } 
+            else {
+
                 card.style.display =
-                    card.getAttribute("data-marca") === marca
-                        ? "block"
-                        : "none";
+                card.getAttribute("data-marca") === marca
+                ? "block"
+                : "none";
+
             }
+
         });
     });
+});
+
+
+/* CARRITO */
+
+const botonesCarrito = document.querySelectorAll(".btn-carrito");
+
+botonesCarrito.forEach(boton => {
+
+    boton.addEventListener("click", () => {
+
+        const producto = {
+
+            nombre: boton.dataset.nombre,
+            img: boton.dataset.img
+
+        };
+
+        let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+        carrito.push(producto);
+
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+        alert("Producto agregado al carrito");
+
+    });
+
 });
